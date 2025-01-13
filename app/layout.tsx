87e62a01c82cba39369f72bad12aaa7079d687b4
Frank Chen <fronk@Frank-the-Tank.local>
2025-01-13 10:34:03 -0800

    change v0 app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,23 @@
-import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
+import type { Metadata } from 'next'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Mise en Money',
+  description: 'Helping service workers master their money mindset, automate their finances, and live their best lives.',
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
+
