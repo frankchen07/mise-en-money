8ce7ed26637e9d512d6b18334a726ef72293806b
Frank Chen <fronk@Frank-the-Tank.local>
2025-01-27 15:28:13 -0800

    updated landing page copy

--- landing-page.tsx
+++ landing-page.tsx
@@ -54,7 +54,7 @@ export default function LandingPage() {
                   Welcome to Mise en Money
                 </h1>
                 <p className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                  I help service workers master their money mindset, automate their finances, and live their best lives. Sign up and get an actionable 5-day plan to take control of your finances.
+                  Mise en Money helps you build an automated financial system aligned with your values so you can spend intentionally, save effortlessy, and live the life you've dreamed of. Sign up today and get an actionable 5-day plan to take control of your finances.
                 </p>
               </div>
               <form onSubmit={handleSubmit} className="flex w-full max-w-sm items-center space-x-2">
@@ -72,7 +72,7 @@ export default function LandingPage() {
                 </Button>
               </form>
               {status === 'success' && (
-                <p className="text-green-600">Thank you! The plan will be sent to your email shortly.</p>
+                <p className="text-green-600">Thanks! A message will be sent to your email shortly.</p>
               )}
               {status === 'error' && (
                 <p className="text-red-600">An error occurred. Please try again later.</p>
@@ -84,9 +84,9 @@ export default function LandingPage() {
           <div className="container px-4 md:px-6 mx-auto">
             <div className="flex flex-col items-center space-y-8 text-center">
               <div className="space-y-6 max-w-[600px]">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Need extra help?</h2>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Get Personalized Coaching</h2>
                 <p className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                  Personal finances can be a DIY thing, but having extra help can be beneficial for accountability, speeding up savings growth, or really getting out of debt. Let's have a chat to see if it's a good fit for us!
+                  Just because I pieced together my own personal finances doesn't mean you have to. Whether you're looking to aggressively grow your savings, pay off some pesky debt, or work out the kinks in a system, personalized coaching can make a difference. Let's chat and see how we can turn your goals into a reality!
                 </p>
               </div>
               <div className="flex flex-col sm:flex-row gap-4">
